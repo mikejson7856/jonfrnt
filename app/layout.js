759eded82e021fd2b87d5abda822608ecc6c3efb
mikejson7856 <163439483+mikejson7856@users.noreply.github.com>
2025-09-21 21:21:48 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Google Maps",
-  description: "google maps",
+  title: "Google Duo",
+  description: "google duo",
 };
 
 export default function RootLayout({ children }) {
